7c44564c117bc4f65aab34a101dae37a2c4fd6ee
mrmrs <mr.adam.morse@gmail.com>
2024-05-23 08:35:55 -0700

    This makes a bunch of changes
    - Adds a bunch of color palettes that can be used to generate images
    - Allows for random color mode
    - Toggles color of outline to be black or darkest color in palette
    - Adds concept of presets
    - Adds save and regen counters

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,44 @@
 <script setup lang="ts">
 import { generateSprite } from "./generateSprite"
+import { palettes } from "./palettes"
+import { presets } from "./presets"
 import { onMounted, ref, watch } from "vue"
+import PalettePreview from './PalettePreview.vue';
 
 const canvasEl = ref<HTMLCanvasElement>()
 const size = ref({ width: 8, height: 16 })
 const zoom = ref(2)
+const countRegen = ref(0)
+const countSave = ref(0)
+const holeThreshold = ref(0.2)
+const drawRectThreshold = ref(0.5)
+const usePalette = ref(true)
+const selectedPaletteIndex = ref(4)
+const selectedPresetIndex = ref(-1) // -1 means no preset is selected
+const useBlackForOutline = ref(true)
+const durableObjectNameRegen = 'COUNTER_VOID_SPRITE_REGEN'
+const durableObjectNameSave = 'COUNTER_VOID_SPRITE_SAVE'
 
-onMounted(() => {
-  updateSprite()
-})
+const fetchCount = async (counterName: string, countRef: any) => {
+  try {
+    const response = await fetch(`https://ts-gen-count.adam-f8f.workers.dev/?name=${counterName}`)
+    const data = await response.text()
+    countRef.value = parseInt(data, 10)
+  } catch (error) {
+    console.error(`Error fetching ${counterName}:`, error)
+  }
+}
 
-watch(
-  () => size.value,
-  () => updateSprite(),
-  { deep: true }
-)
+const handleIncrement = async (counterName: string) => {
+  try {
+    await fetch(`https://ts-gen-count.adam-f8f.workers.dev/increment?name=${counterName}`, {
+      method: 'POST',
+    })
+    fetchCount(counterName, counterName === durableObjectNameRegen ? countRegen : countSave)
+  } catch (error) {
+    console.error(`Error incrementing ${counterName}:`, error)
+  }
+}
 
 const updateSprite = () => {
   const canvas = canvasEl.value
@@ -26,10 +50,25 @@ const updateSprite = () => {
   canvas.height = size.value.height
 
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  generateSprite(ctx, size.value)
+
+  if (selectedPresetIndex.value >= 0) {
+    generateSprite(ctx, size.value, selectedPresetIndex.value)
+  } else {
+    const customPreset = {
+      holeThreshold: holeThreshold.value,
+      drawRectThreshold: drawRectThreshold.value,
+      paletteIndex: selectedPaletteIndex.value,
+      useBlackForOutline: useBlackForOutline.value,
+      customLogic: (x, y, context, color) => {
+        context.fillStyle = color;
+        context.fillRect(x, y, 1, 1);
+      },
+    };
+    generateSprite(ctx, size.value, -1, customPreset)
+  }
 }
 
-const save = (event: Event) => {
+const save = async (event: Event) => {
   const canvas = canvasEl.value
   if (!canvas) throw new Error("Canvas element not found")
   const dataUrl = canvas.toDataURL("image/png")
@@ -38,36 +77,101 @@ const save = (event: Event) => {
   a.download = "sprite.png"
   a.click()
   event.preventDefault()
+  await handleIncrement(durableObjectNameSave) // Increment the count when save is clicked
 }
+
+const regenerate = async () => {
+  updateSprite()
+  await handleIncrement(durableObjectNameRegen) // Increment the count when regenerate is clicked
+}
+
+onMounted(() => {
+  updateSprite()
+  fetchCount(durableObjectNameRegen, countRegen) // Fetch the initial count for regenerations
+  fetchCount(durableObjectNameSave, countSave) // Fetch the initial count for saves
+})
+
+watch(
+  () => [size.value, holeThreshold.value, drawRectThreshold.value, usePalette.value, selectedPaletteIndex.value, useBlackForOutline.value, selectedPresetIndex.value],
+  () => {
+    updateSprite()
+  },
+  { deep: true }
+)
 </script>
 
 <template>
-  <div>
-    <header style='border-bottom: 1px solid;padding-left: 1rem; padding-right: 1rem; display: flex; align-items: center; height: 32px;'>
-      <h1 style='margin: 0; line-height: 1; font-size: 10px;white-space:nowrap;margin-right: 2rem; height: 100%; display: inline-flex; align-items: center; border-right: 1px solid currentColor; padding-right: 1rem;'>Endless Sprites</h1>
-      <form style='display: flex; align-items: center; justify-content: space-between; font-size:12px; width: 100%; gap: 2rem;'>
+  <div style='height: 100dvh; display: grid; grid-template-columns: minmax(320px, 1fr) 3fr;'>
+    <aside style='border-right: 1px solid; position: relative;'>
+      <h1 style='border-bottom: 1px solid; margin: 0; padding: 8px 16px; font-size: 10px;'>Endless Sprites</h1>
+      <form style='padding: 16px; width: 100%; display: block;'>
         <label style='display: flex; align-items: center; gap: 4px;' title='Width'>
           <span style='font-size: 10px;'>w</span> 
-          <input type="range" v-model="size.width" min='0' max='512' />
+          <input type="range" v-model="size.width" min='0' max='512' style='width:100%;'/>
           <input type="number" v-model="size.width" style='max-width: 6ch; font-size: 10px;'/>
         </label>
-        <label style='display: flex; align-items: center; gap: 4px;' title='Height'>
+        <label style='display: flex; justify-content: space-between; align-items: center; gap: 4px;' title='Height'>
           <span style='font-size: 10px;'>h</span> 
-          <input type="range" v-model="size.height" min='1' max='1440' style='font-size: 10px;'/>
+          <input type="range" v-model="size.height" min='1' max='1440' style='font-size: 10px; width: 100%;'/>
           <input type="number" v-model="size.height" style='max-width: 6ch; font-size: 10px;'/>
         </label>
-        <label style='font-size: 10px; display: flex; align-items: center; gap: 8px;'><span style='font-size: 10px;'>zoom</span> <input type="range" min='1' max='64' step='1' v-model="zoom" />{{ zoom }}</label>
-        <button type="button" @click="updateSprite" style='margin-left: auto; background: black; appearance: none; color: white; border: 0; padding: 4px 12px; font-size: 10px; cursor: pointer;'>Regenerate</button>
-        <button @click="save" style='display: inline-block; background: black; appearance: none; color: white; margin: 0; border: 0; padding: 4px 12px; font-size: 10px; cursor: pointer;'>Save</button>
+        <label style='font-size: 10px; display: flex; align-items: center; gap: 8px;'>
+          <span style='font-size: 10px;'>zoom</span> 
+          <input type="range" min='1' max='64' step='1' v-model="zoom" style='width:100%;'/>
+          {{ zoom }}
+        </label>
+        <label style='font-size: 10px; display: flex; align-items: center; gap: 8px;'>
+          <span style='font-size: 10px;'>hole</span> 
+          <input type="range" min='0' max='1' step='0.01' v-model="holeThreshold" style='width:100%;'/>
+          {{ holeThreshold }}
+        </label>
+        <label style='font-size: 10px; display: flex; align-items: center; gap: 8px;'>
+          <span style='font-size: 10px;'>drawRect</span> 
+          <input type="range" min='0' max='1' step='0.01' v-model="drawRectThreshold" style='width:100%;'/>
+          {{ drawRectThreshold }}
+        </label>
+        <div style='display: flex; justify-content: space-between; margin-top: 32px;'>
+          <label style='font-size: 10px; display: flex; align-items: center; gap: 8px;'>
+            <span style='font-size: 10px;'>🎨 Palette</span> 
+            <input type="checkbox" v-model="usePalette" />
+          </label>
+          <label style='font-size: 10px; display: flex; align-items: center; gap: 8px;'>
+            <span style='font-size: 10px;'>Black Outline</span> 
+            <input type="checkbox" v-model="useBlackForOutline" />
+          </label>
+        </div>
+        <div style='max-height: 256px; overflow: scroll; border: 1px solid;'>
+          <div v-for="(palette, index) in palettes" :key="index">
+            <PalettePreview :palette="palette" :isSelected="index === selectedPaletteIndex" @select="selectedPaletteIndex = index" />
+          </div>
+        </div>
+        <div style='margin-top: 16px;'>
+          <label style='font-size: 10px; display: block;'>Preset</label>
+          <select v-model="selectedPresetIndex" style='width: 100%; font-size: 10px;'>
+            <option :value="-1">Custom</option>
+            <option v-for="(preset, index) in presets" :key="index" :value="index">
+              {{ preset.name }}
+            </option>
+          </select>
+        </div>
+        <div style='margin-top: 48px; display: flex; gap: 8px;'>
+          <button @click="save" style='display: inline-block; background: black; appearance: none; color: white; border: 0; padding: 4px 12px; font-size: 10px; cursor: pointer;'>Save</button>
+          <button type="button" @click="regenerate" style='background: blue; appearance: none; color: white; border: 0; padding: 4px 12px; font-size: 10px; cursor: pointer;'>Regenerate</button>
+        </div>
       </form>
-    </header>
-    <main class='bg-checkered' style='padding: 64px;'>
+      <footer style="background: white; position: absolute; bottom: 0; left: 0; right: 0; font-size: 10px; padding: 8px 16px;">
+        <p style='margin: 0; padding: 0;'>Regenerations: {{ countRegen }}</p>
+        <p style='margin-top: 8px; padding: 0;'>Saves: {{ countSave }}</p>
+      </footer>
+    </aside>
+    <main class='bg-checkered' style='padding: 64px; height: calc(100dvh - 33px);'>
       <canvas ref="canvasEl" :style="{ zoom: 3 * zoom }" />
     </main>
   </div>
 </template>
 
 <style>
+* { box-sizing: border-box; }
 html, body {
   margin: 0;
   font-family: monospace!important;
--- src/generateSprite.ts
+++ src/generateSprite.ts
@@ -1,24 +1,25 @@
 // Adapted from ZzSprite by Frank Force 2020, Corey doesn't fully understand how this code works and wants to make it more readable!
 
 import { palettes } from './palettes';
+import { presets } from './presets';
 
 export const generateSprite = (
   context: CanvasRenderingContext2D,
   size: { width: number; height: number },
-  holeThreshold: number,
-  drawRectThreshold: number,
-  usePalette: boolean,
-  paletteIndex: number,
-  useBlackForOutline: boolean,
+  presetIndex: number,
+  customPreset: any = {},
   seed?: number
 ) => {
-  seed ??= Math.floor(Math.random() * 1e9)
-  const center = Math.floor(size.width / 2)
-  const doubleCenterOffset = size.width % 2 == 0 ? 1 : 0
+  seed ??= Math.floor(Math.random() * 1e9);
+  const preset = presetIndex >= 0 ? presets[presetIndex] : customPreset;
+  const { holeThreshold, drawRectThreshold, paletteIndex, useBlackForOutline, customLogic } = preset;
 
-  let color: string | undefined = undefined
-  const randomColor = makeRNG(seed)
-  const palette = palettes[paletteIndex]?.colors.map(color => `#${color}`)
+  const center = Math.floor(size.width / 2);
+  const doubleCenterOffset = size.width % 2 == 0 ? 1 : 0;
+
+  let color: string | undefined = undefined;
+  const randomColor = makeRNG(seed);
+  const palette = palettes[paletteIndex]?.colors.map(color => `#${color}`);
 
   // Function to calculate luminance
   const getLuminance = (hex: string) => {
@@ -36,47 +37,40 @@ export const generateSprite = (
   }, palette[0]) || 'black';
 
   ;["outline", "fill"].forEach((drawType) => {
-    const random = makeRNG(seed)
+    const random = makeRNG(seed);
 
     for (let y = 0; y < size.height - 2; ++y) {
       for (let x = 0; x < (size.width - 1) / 2; ++x) {
         if (!color || randomColor() < 0.05) {
-          if (usePalette && palette) {
-            color = palette[Math.floor(randomColor(palette.length))]
-          } else {
-            const hue = Math.floor(randomColor(360))
-            color = `hsl(${hue},${randomColor(90, 50) | 0}%,${randomColor(80, 50) | 0}%)`
-          }
+          color = palette[Math.floor(randomColor(palette.length))];
         }
 
-        const hole = random() < holeThreshold
-        const drawRect = random() < drawRectThreshold
+        const hole = random() < holeThreshold;
+        const drawRect = random() < drawRectThreshold;
 
         if (drawRect && !hole) {
           if (drawType == "outline") {
-            context.fillStyle = useBlackForOutline ? "black" : darkestColor
-            context.fillRect(center + x - doubleCenterOffset - 1, y, 3, 3)
+            context.fillStyle = useBlackForOutline ? "black" : darkestColor;
+            context.fillRect(center + x - doubleCenterOffset - 1, y, 3, 3);
             if (doubleCenterOffset) {
-              context.fillRect(center - x - 1, y, 3, 3)
+              context.fillRect(center - x - 1, y, 3, 3);
             } else {
-              context.fillRect(center - x - doubleCenterOffset - 1, y, 3, 3)
+              context.fillRect(center - x - doubleCenterOffset - 1, y, 3, 3);
             }
           } else {
-            context.fillStyle = color
-
-            context.fillRect(center + x - doubleCenterOffset, y + 1, 1, 1)
-            context.fillRect(center - x, y + 1, 1, 1)
+            customLogic(center + x - doubleCenterOffset, y + 1, context, color);
+            customLogic(center - x, y + 1, context, color);
           }
         }
       }
     }
-  })
-}
+  });
+};
 
 const makeRNG = (randomSeed: number) => {
   return (max = 1, min = 0) => {
-    randomSeed ^= randomSeed << 13
-    randomSeed ^= randomSeed >>> 17
-    return ((Math.abs((randomSeed ^= randomSeed << 5)) % 1e9) / 1e9) * (max - min) + min
-  }
-}
+    randomSeed ^= randomSeed << 13;
+    randomSeed ^= randomSeed >>> 17;
+    return ((Math.abs((randomSeed ^= randomSeed << 5)) % 1e9) / 1e9) * (max - min) + min;
+  };
+};
